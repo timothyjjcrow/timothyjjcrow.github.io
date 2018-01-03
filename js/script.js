e87da0025b70015b8084b40c30245a654af75c34
Tim Crowley <test@tests-MacBook-Pro.local>
2018-01-02 18:31:08 -0800

    initial commit of portfolio site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+	Typed.new("#typed", {
+		strings: ["I build websites.", "I DJ parties.", "I develop software.", "I study Computer Engineering."],
+		startDelay: 500,
+		backDelay: 2000,
+		contentType: 'text',
+		backSpeed: 30,
+		typeSpeed: 40,
+		loop: null
+	});
+});
