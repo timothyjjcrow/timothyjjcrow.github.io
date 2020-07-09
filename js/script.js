a5ad99261487fd0b71d5d1e358788a6bbc7fb9e1
Tim Crowley <timothyjjcrow@gmail.com>
2020-07-09 14:36:22 -0700

    chrip added to text input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 	Typed.new("#typed", {
-		strings: ["I build websites.", "I make music.", "I develop software.", "Press space to play Block Stacker."],
+		strings: ["I build websites.", "I make music.", "I develop software.", "Press space to play Block Stacker.", "reach Lvl 25 to win!"],
 		startDelay: 500,
 		backDelay: 2000,
 		contentType: 'text',
